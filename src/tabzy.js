b12a7fc789395e14555d12b13f9bcfddcc24d74b
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2025-03-30 16:59:40 +0700

    feat: tabzy basic with some prototype

---
+++ src/tabzy.js
@@ -0,0 +1,99 @@
+Tabzy.prototype.destroy = function () {
+    this.container.innerHTML = this.original;
+    this.container = null;
+    this.panels.forEach((panel) => (panel.hidden = false));
+    this.panels = null;
+    this.tabs = null;
+    this.original = null;
+};
+
+Tabzy.prototype.switchTab = function (target) {
+    let tabActive = null;
+    if (typeof target === "string") {
+        tabActive = this.tabs.find((tab) => tab.getAttribute("href") === target);
+    } else if (this.tabs.includes(target)) tabActive = target;
+
+    if (!tabActive) {
+        console.error("Element not found");
+        return;
+    }
+
+    this._setActiveTab(tabActive);
+};
+
+Tabzy.prototype._setActiveTab = function (tabActive) {
+    // Xoá bỏ style của các class li
+    this.tabs.forEach((tab) => tab.closest("li").classList.remove("tabzy--active"));
+    // Thêm class active cho tab hiện tại
+    tabActive.closest("li").classList.add("tabzy--active");
+    // Ẩn các panel đi
+    this.panels.forEach((panel) => (panel.hidden = true));
+    const panelActive = document.querySelector(tabActive.getAttribute("href"));
+    // Hiển thị panel tương ứng
+    panelActive.hidden = false;
+    localStorage.setItem("tabActive", tabActive.getAttribute("href"));
+};
+
+Tabzy.prototype._init = function () {
+    let tabActive = this.tabs[0];
+    const idTabActiveLocal = localStorage.getItem("tabActive");
+    if (idTabActiveLocal) {
+        const tabActiveLocal = this.tabs.find(
+            (tab) => tab.getAttribute("href") === idTabActiveLocal
+        );
+        if (tabActiveLocal) tabActive = tabActiveLocal;
+    }
+
+    this._setActiveTab(tabActive);
+    // Add events
+    this.tabs.forEach((tab) => {
+        tab.onclick = (event) => this._handleTabClick(event, tab);
+    });
+};
+
+Tabzy.prototype._handleTabClick = function (event, tabActive) {
+    event.preventDefault();
+    this._setActiveTab(tabActive);
+};
+
+function Tabzy(selector, options) {
+    this.selector = selector;
+    this.options = options ?? {};
+    this.container = document.getElementById(this.selector);
+    if (!this.container) {
+        console.error(`Element with selector ${this.selector} not found`);
+        return;
+    }
+
+    this.original = this.container.innerHTML;
+
+    this.tabs = Array.from(this.container.querySelectorAll("li a"));
+
+    if (!this.tabs.length) {
+        console.error(`No tabs found in ${this.selector}`);
+        return;
+    }
+
+    this.panels = this.tabs
+        .map((tab) => {
+            const panel = document.querySelector(tab.getAttribute("href"));
+            if (!panel) {
+                console.error(`Panel with selector ${tab.getAttribute("href")} not found`);
+                return;
+            }
+
+            return panel;
+        })
+        .filter(Boolean);
+
+    if (this.panels.length !== this.tabs.length) {
+        console.error("Number of tabs and panels do not match");
+        return;
+    }
+
+    this._init();
+}
+
+const tabzy = new Tabzy("tabs", {
+    remberTab: true,
+});
